d27be9fe419934a3ebcf35065fe6f9d241f8f5d2
snacktrapp <trappbrian@gmail.com>
2026-06-27 19:19:01 -0700

    Route RAT page through public site

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/rat",
+        destination: "https://proof.verifiedeffort.com/rat",
+      },
+      {
+        source: "/rat/:path*",
+        destination: "https://proof.verifiedeffort.com/rat/:path*",
+      },
+      {
+        source: "/_app/:path*",
+        destination: "https://proof.verifiedeffort.com/_app/:path*",
+      },
+    ];
+  },
   async redirects() {
     return [
       {
