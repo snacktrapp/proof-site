f743d127a08c84c386f382235ab45678ea1837b7
snacktrapp <team@beelinetech.co>
2026-06-27 20:16:03 -0700

    Make RAT native to public site (#70)

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/rat",
-        destination: "https://proof.verifiedeffort.com/rat",
-      },
-      {
-        source: "/rat/:path*",
-        destination: "https://proof.verifiedeffort.com/rat/:path*",
-      },
-      {
-        source: "/_app/:path*",
-        destination: "https://proof.verifiedeffort.com/_app/:path*",
-      },
-    ];
-  },
   async redirects() {
     return [
       {
