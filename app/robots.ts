35b17edd6a72935e61e18d7ac9688c3f83d5c9f9
snacktrapp <trappbrian@gmail.com>
2026-07-08 05:29:36 -0700

    Hide public demo walkthrough

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/"],
+        disallow: ["/api/", "/demo", "/demo/"],
       },
     ],
     sitemap: "https://verifiedeffort.com/sitemap.xml",
